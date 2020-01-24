01c5b5119d347a2a2bbcd5790137ed8baf3cd325
DanielLaszczych <daniellaszczych@gmail.com>
2020-01-24 01:10:27 -0500

    Navigation changes

--- client/src/components/PitNavigation.js
+++ client/src/components/PitNavigation.js
@@ -1,86 +1,86 @@
-import React, { Component } from "react";
-import "./PitContent.css";
-import Logo from "./1796NumberswithScratch.png";
-import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
-import Table from "react-bootstrap/Table";
-import Button from "react-bootstrap/Button";
+import React, { Component } from 'react';
+import './PitContent.css';
+import Logo from './1796NumberswithScratch.png';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
 class PitNavigation extends Component {
   state = {
     validated: false,
-    widthSize: "",
-    heightSize: "",
-    competition: "",
+    widthSize: '',
+    heightSize: '',
+    competition: '',
     tableData: {}
   };
 
   componentDidMount() {
-    fetch("/pitNav")
+    fetch('/pitNav')
       .then(response => response.json())
       .then(data => {
         console.log(data);
         this.setState({ Tabledata: data });
       })
       .catch(error => {
-        console.error("Error:", error);
+        console.error('Error:', error);
       });
     this.setState({
-      widthSize: window.innerWidth <= 760 ? "90%" : "50%"
+      widthSize: window.innerWidth <= 760 ? '90%' : '50%'
     });
-    this.setState({ heightSize: window.innerHeight + "px" });
+    this.setState({ heightSize: window.innerHeight + 'px' });
   }
 
   handleRefresh = () => {};
 
   render() {
     return (
-      <div className="div-main">
-        <div className="justify-content-center">
+      <div className='div-main'>
+        <div className='justify-content-center'>
           <img
-            alt="Logo"
+            alt='Logo'
             src={Logo}
             style={{
-              width: this.state.widthSize === "90%" ? "70%" : "30%",
-              marginTop: "20px",
-              marginLeft: "10px"
+              width: this.state.widthSize === '90%' ? '70%' : '30%',
+              marginTop: '20px',
+              marginLeft: '10px'
             }}
           />
         </div>
-        <div style={{ width: this.state.widthSize }} className="div-second">
-          <div className="pit-form">
-            <div className="div-form">
-              <Form.Group style={{ width: "80%", marginLeft: "1%" }} as={Row}>
+        <div style={{ width: this.state.widthSize }} className='div-second'>
+          <div className='pit-form'>
+            <div className='div-form'>
+              <Form.Group style={{ width: '80%', marginLeft: '1%' }} as={Row}>
                 <Form.Label
-                  className="mb-1"
+                  className='mb-1'
                   style={{
-                    fontFamily: "Helvetica, Arial",
-                    fontSize: "110%"
+                    fontFamily: 'Helvetica, Arial',
+                    fontSize: '110%'
                   }}
                 >
                   Competition:
                 </Form.Label>
               </Form.Group>
               <Form.Group
-                controlId="formCompetition"
-                style={{ width: "80%", marginLeft: "2%" }}
+                controlId='formCompetition'
+                style={{ width: '80%', marginLeft: '2%' }}
                 as={Row}
               >
                 <Form.Control
                   style={{
-                    background: "none",
-                    fontFamily: "Helvetica, Arial"
+                    background: 'none',
+                    fontFamily: 'Helvetica, Arial'
                   }}
-                  className="mb-1"
-                  as="select"
+                  className='mb-1'
+                  as='select'
                   onChange={this.handleGroupChange}
                 ></Form.Control>
               </Form.Group>
               <Button
-                type="btn"
+                type='btn'
                 onClick={this.handleRefresh}
-                className="btn-lg"
-                style={{ fontFamily: "Helvetica, Arial" }}
+                className='btn-lg'
+                style={{ fontFamily: 'Helvetica, Arial' }}
               >
                 Refresh
               </Button>
--- routes/pitNavRouter.js
+++ routes/pitNavRouter.js
@@ -1,14 +1,14 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const db = require("../db");
+const db = require('../db');
 
-router.get("/pitNav", (req, res) => {
-  console.log(req);
+router.get('/pitNav', (req, res) => {
+  //   console.log(req);
   let returnData;
   //   let params = req.body;
   const getPitTableQuery =
-    "SELECT t.num, t.name, p.status FROM pit p INNER JOIN team t ON t.team_id=p.team_id INNER JOIN competition c ON c.competition_id=p.competition_id WHERE c.name = $1;";
-  const getPitTableValues = ["HVR"];
+    'SELECT t.num, t.name, p.status FROM pit p INNER JOIN team t ON t.team_id=p.team_id INNER JOIN competition c ON c.competition_id=p.competition_id WHERE c.name = $1;';
+  const getPitTableValues = ['HVR'];
 
   db.query(getPitTableQuery, getPitTableValues)
     .then(res => {
